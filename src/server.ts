0ed6e33a1bb0cea2c3f473c3fec48f5e76f1dcc4
nahidraz23 <nzamanraz@gmail.com>
2026-02-07 17:35:42 +0600

    chore: app.listen removed

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 5001;
+// const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 export default app;
 
